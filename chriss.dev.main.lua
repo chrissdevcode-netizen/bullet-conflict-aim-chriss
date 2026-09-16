b4cb995545f45f140c52ea10e79603521cf4524d
chrissdevcode-netizen <chrissdev.code@gmail.com>
2026-09-15 22:09:40 -0600

    Update chriss.dev.main.lua

--- chriss.dev.main.lua
+++ chriss.dev.main.lua
@@ -916,6 +916,22 @@ local function ObtenerEnemigoMasCercano()
     return objetivoCercano
 end
 
+-- LOGICA WALL CHECK
+local function VerificarParedVisibilidad(objetivoParte)
+    if not Config.WallCheck then return true end 
+    
+    local origen = Camera.CFrame.Position
+    local destino = objetivoParte.Position
+    local raycastParams = RaycastParams.new()
+    
+    raycastParams.FilterType = Enum.RaycastFilterType.Exclude
+    raycastParams.FilterDescendantsInstances = {LocalPlayer.Character, objetivoParte.Parent}
+
+    local resultado = workspace:Raycast(origen, destino - origen, raycastParams)
+    return resultado == nil 
+end
+
+
 -- LOGICA FOV & AIMBOT MAIN
 RunService.RenderStepped:Connect(function()
     if not Camera or not workspace.CurrentCamera then
